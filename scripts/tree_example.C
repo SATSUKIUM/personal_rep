418415e13953d708e8dac393f69b4ba6468c1db0
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2025-04-12 16:12:00 +0900

    commit

--- scripts/tree_example.C
+++ scripts/tree_example.C
@@ -29,6 +29,33 @@
 #include <TChainElement.h>
 #include <TObjArray.h>
 
-Double_t tree_example(){
+#include <TFile.h>
+#include <TRandom.h> // 乱数
 
+Double_t tree_example(){
+    // 保存ファイルと、その中に記録するツリー
+    TFile *file = new TFile("tree.root", "RECREATE");
+    TTree *tree = new TTree("tree", "example discription");
+
+    // 各イベントごとの値を入れる変数
+    Int_t time;
+    Double_t energy_deposit[2];
+
+    // ツリーにブランチを作る
+    tree->Branch("time", &time, "time/I");
+    tree->Branch("energy_deposit", energy_deposit, "energy_deposit/D");
+
+    // イベントごとに値を記録
+    for(Int_t eventID=0; eventID<1000; eventID++){
+        time = 2*eventID; // 例
+        energy_deposit[0] = gRandom->Poisson(3);
+        energy_deposit[1] = gRandom->Poisson(5);
+
+        tree->Fill();
+    }
+
+    // ツリーを書き出し
+    tree->Write();
+    file->Close();
+    
 }
